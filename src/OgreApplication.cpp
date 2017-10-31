8f1f6f9bb0aa2d1751df10df3f46623feace7a03
gapry <gapry.mo@gmail.com>
2017-11-01 02:56:53 +0800

    separate the entry point from user data-type.

--- src/OgreApplication.cpp
+++ src/OgreApplication.cpp
@@ -8,16 +8,3 @@ void mgaf::OgreApplication::createScene(void)
 mgaf::OgreApplication::OgreApplication(void) {}
 
 mgaf::OgreApplication::~OgreApplication(void) {}
-
-int main(int argc, char *argv[])
-{
-    mgaf::OgreApplication app;
-
-    try {
-        app.go();
-    } catch(Ogre::Exception& e)  {
-        std::cerr << "An exception has occurred: " << e.getFullDescription().c_str() << std::endl;
-    }
-
-    return 0;
-}
